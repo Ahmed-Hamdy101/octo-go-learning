c03ad2e28b79891193949385490294d01ab3fc34
Ahmed-Hamdy101 <ahmedhamdy.mh95@gmail.com>
2026-09-08 22:58:35 +0200

    feat(ch2): add palindrome.go with variable declarations and print statement

---
+++ ch2/gowork/src/palindrome/palindrome.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// initial value static type
+	var str string = "Hello World!"
+	// define variable which will work with `any`
+	NumberTwo := 2
+	// define integer
+	var age int = 30
+	// define float
+	var pi float32 = 3.14
+	// define boolean
+	var isTrue bool = true
+
+	fmt.Println(NumberTwo, str, age, pi, isTrue)
+
+}
