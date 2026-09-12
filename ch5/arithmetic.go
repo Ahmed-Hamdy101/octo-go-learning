eff41445fd5c0d427977feb7c1af7747a0f5f44f
Ahmed-Hamdy101 <ahmedhamdy.mh95@gmail.com>
2026-09-13 00:21:27 +0200

    fix(ch5): improve comments and formatting in arithmetic.go

--- ch5/arithmetic.go
+++ ch5/arithmetic.go
@@ -6,18 +6,18 @@ import (
 )
 
 func main() {
-	//more variables
+	// More variables.
 	i1, i2, i3 := 1, 2, 3
-	// addtion
+	// Addition.
 	total := i1 + i2 + i3
 	fmt.Println("total:", total)
-	// import arithmatic operation
+	// Use math/big for higher-precision arithmetic.
 	var b1, b2, b3, bigSum big.Float
 	// float
 	b1.SetFloat64(23.4)
 	b2.SetFloat64(13.4)
 	b3.SetFloat64(53.4)
 	bigSum.Add(&b1, &b2).Add(&bigSum, &b3)
-	fmt.Printf("Big sum = %.10g\n", bigSum)
+	fmt.Printf("Big sum = %s\n", bigSum.Text('g', 10))
 
 }
